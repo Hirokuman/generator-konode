024494ee177247275d863eb87b9fee1bfb6537c1
HirokazuKumamoto <hirokuma5@gmail.com>
2015-04-16 20:27:03 +0900

    bug fix

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -102,7 +102,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -111,5 +110,5 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['watch']);
   grunt.registerTask('compile', ['ts:app', 'ts:script']);
   grunt.registerTask('resolve', ['bower:install', 'concat:js', 'concat:css', 'copy:fonts', 'copy:cssmap', 'ts:default']);
-  grunt.registerTask('build', ['jshint', 'clean', 'copy:dist', 'uglify', 'cssmin']);
+  grunt.registerTask('build', ['clean', 'copy:dist', 'uglify', 'cssmin']);
 };
